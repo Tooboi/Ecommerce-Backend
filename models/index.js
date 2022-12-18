8050488983e055a3c5a8a633fa42b577bb40ee9c
Tooboi <japica123@gmail.com>
2022-12-18 17:14:11 -0500

    Creating models

--- models/index.js
+++ models/index.js
@@ -5,9 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: ''
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  
+})
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
